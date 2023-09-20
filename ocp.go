3984810038e9b891e10da5f1eef3e862e1840170
emannuel <emannuelloxt@hotmail.com>
2023-09-19 23:02:19 -0400

    feat(ocp): open for extension and closed for modification

---
+++ ocp.go
@@ -0,0 +1,98 @@
+package main
+
+// Open Closed Principle
+
+type Color int
+
+const (
+	red Color = iota
+	green
+	blue
+)
+
+type Size int
+
+const (
+	small Size = iota
+	medium
+	large
+)
+
+type Product struct {
+	name  string
+	color Color
+	size  Size
+}
+
+type Filter struct {
+}
+
+func (f *Filter) FilterByColor(
+	products []Product, color Color) []*Product {
+	result := make([]*Product, 0)
+
+	for i, v := range products {
+		if v.color == color {
+			result = append(result, &products[i])
+		}
+	}
+
+	return result
+}
+
+type Specification interface {
+	IsSatisfied(p *Product) bool
+}
+
+type ColorSpecification struct {
+	color Color
+}
+
+func (spec ColorSpecification) IsSatisfied(p *Product) bool {
+	return p.color == spec.color
+}
+
+type SizeSpecification struct {
+	size Size
+}
+
+func (spec SizeSpecification) IsSatisfied(p *Product) bool {
+	return p.size == spec.size
+}
+
+type BetterFilter struct{}
+
+func (f *BetterFilter) Filter(products []Product, spec Specification) []*Product {
+	result := make([]*Product, 0)
+
+	for i, v := range products {
+		if spec.IsSatisfied(&v) {
+			result = append(result, &products[i])
+		}
+	}
+
+	return result
+}
+
+func main() {
+	apple := Product{"Apple", green, small}
+	tree := Product{"Tree", green, large}
+	house := Product{"House", blue, large}
+
+	products := []Product{apple, tree, house}
+
+	f := Filter{}
+	greenProducts := f.FilterByColor(products, green)
+
+	for _, v := range greenProducts {
+		println(v.name, "is green")
+	}
+
+	greenSpec := ColorSpecification{green}
+	bf := BetterFilter{}
+	greenProducts = bf.Filter(products, greenSpec)
+
+	for _, v := range greenProducts {
+		println(v.name, "is green")
+	}
+}
